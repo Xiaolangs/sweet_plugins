9ca705a1fa7cb6b1c5678bd5639ca62f37278ac6
小狼狼 <1036046880@qq.com>
2024-11-17 23:48:52 +0800

    Initial commit

---
+++ plugins/src/main/java/com/sweet/hook/storage/CoreStorage.kt
@@ -0,0 +1,32 @@
+package com.sweet.hook.storage
+
+/**
+ * description:
+ * date: 2024/11/17 22:06
+ * author: xiaolang
+ */
+class CoreStorage {
+    //6 phone
+    fun getWxOldId(): String {
+        return ""
+    }
+
+    fun getWxId(): String {
+        return ""
+    }
+
+    fun getPhone(): String {
+        return ""
+    }
+
+    fun getSex(): Int {
+        return -1
+    }
+
+    fun getNickName(): String {
+        return ""
+    }
+    fun getSign():String{
+        return ""
+    }
+}
---
+++ plugins/src/main/java/com/sweet/plugins/PluginsUtils.kt
@@ -0,0 +1,10 @@
+package com.sweet.plugins
+
+/**
+ * description:
+ * date: 2024/11/17 22:08
+ * author: xiaolang
+ */
+object PluginsUtils {
+    fun getWxClassLoader(): ClassLoader? = null
+}
