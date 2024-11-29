dfa73f0c13a22b5c7325057f657988d7fed11557
小狼狼 <1036046880@qq.com>
2024-11-29 11:47:42 +0800

    1.0.14

--- app/src/main/java/com/sweet/plugins/InitHooker.kt
+++ app/src/main/java/com/sweet/plugins/InitHooker.kt
@@ -7,6 +7,7 @@ import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.param.PackageParam
 import com.sweet.ext.scope
 import com.sweet.hook.base.BaseIHookXposedInit
+import com.sweet.hook.storage.CoreStorage
 import com.sweet.utils.HttpUtils
 import com.sweet.utils.LogKTX
 
@@ -18,25 +19,11 @@ import com.sweet.utils.LogKTX
  */
 class InitHooker : BaseIHookXposedInit() {
     override fun PackageParam.load() {
-        Log.e("PluginTest", "test: ------2----$curPluginsHelper")
-
-        "com.tencent.mm.ui.chatting.viewitems.l0".toClass(PluginsUtils.getWxClassLoader()).method {
-            name("a")
-        }.hook {
-            after {
-                //   w3Var.c(1, 0, 1, rVar.f396394d.getString(R.string.ajf), R.raw.icons_filled_eyes_off_new);
-                args[0]?.current()?.method {
-                    name("c")
-                }?.call(
-                    1, 0, 1, "哑巴", android.R.drawable.btn_radio
-                )
-                scope {
-                    Log.e("PluginTest", "test: ------4")
-                    HttpUtils.get<String>("https://applet.xiaojieyahu.top/tianluojson/wx_config53.json").onSuccess {
-                        Log.e("PluginTest", "test: ------$it")
-                    }
-                }
-            }
+        ///这里面就可以开始的插件之旅
+        curPluginsHelper?.injectPluginAppResources()
+        CoreStorage.getWxId()
+        scope {
+            HttpUtils.get<String>("").onSuccess { }.onFailure { }
         }
     }
 }
---
+++ plugins/src/main/java/com/sweet/hook/chat/utils/MsgTools.kt
@@ -0,0 +1,14 @@
+package com.sweet.hook.chat.utils
+
+/**
+ * description:
+ * date: 2024/9/13 14:52
+ * author: xiaolang
+ */
+object MsgTools {
+
+    fun sendTextMsg(sendUserName: String, content: String) {
+
+    }
+
+}
