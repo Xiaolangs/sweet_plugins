a4b38a0fb9c9661b82fc451e90551ecc1f6d305c
小狼狼 <1036046880@qq.com>
2024-11-29 13:57:44 +0800

    1.0.15

---
+++ plugins/src/main/java/com/sweet/utils/AppUtils.kt
@@ -0,0 +1,23 @@
+package com.sweet.utils
+
+import android.app.Activity
+import android.content.Context
+
+
+
+/**
+ * description:
+ * date: 2024/8/6 00:54
+ * author: xiaolang
+ */
+object AppUtils {
+
+
+    fun getTopActivity(): Activity? {
+        return null
+    }
+
+    fun relaunchApp(context: Context, isKillProcess: Boolean = false) {
+
+    }
+}
